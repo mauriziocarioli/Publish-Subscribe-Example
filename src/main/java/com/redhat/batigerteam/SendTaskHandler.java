4eb4e9ab83f45cf8812e24e5f62faf98b33636e8
pamAdmin <>
2019-10-24 16:51:38 -0400

    {/src/main/java/com/redhat/batigerteam/SendTaskHandler.java}

--- src/main/java/com/redhat/batigerteam/SendTaskHandler.java
+++ src/main/java/com/redhat/batigerteam/SendTaskHandler.java
@@ -19,17 +19,29 @@ package com.redhat.batigerteam;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
+import org.kie.api.runtime.process.ProcessRuntime;
+import org.kie.api.runtime.KieSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SendTaskHandler implements WorkItemHandler {
     
     private static final Logger logger = LoggerFactory.getLogger(SendTaskHandler.class);
+    
+    private ProcessRuntime ksession;
+    
+    public SendTaskHandler(KieSession ksession) {
+        this.ksession = ksession;
+    }
+    
+    public setKnowledgeRuntime(KieSession ksession) {
+        this.ksession = ksession;
+    }
 
     public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
         String message = (String) workItem.getParameter("Message");
         logger.debug("Sending message: {}", message);
-        //TODO actually send message
+        ksession.sendSignal("Send Task",message);
         manager.completeWorkItem(workItem.getId(), null);
     }
 
